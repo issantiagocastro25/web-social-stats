841d47df2f184ed913f27d07820144baaae04df1
andres-alfonso <falfonso@windowschannel.com>
2024-08-01 11:18:09 -0500

    Modified styles loginForm

--- src/app/Auth/login/page.tsx
+++ src/app/Auth/login/page.tsx
@@ -1,5 +1,15 @@
 import Login from "@/app/Components/AuthComponents/login";
 
 export default function LoginPage() {
-    return <div className="login"><Login /></div>;
+    return (
+        <>
+            <div className="bg-[#f6f3fa]">
+                <div className="container mx-auto p-4 flex items-center justify-center h-screen">
+                    <div className="max-w-md w-full">
+                        <Login />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
--- src/app/Components/AuthComponents/login.tsx
+++ src/app/Components/AuthComponents/login.tsx
@@ -2,26 +2,34 @@ import React from "react";
 import { Button, Card, Checkbox, Label, TextInput } from "flowbite-react";
 
 export default function Login() {
-    return <Card className="max-w-lg">
-    <form className="flex flex-col gap-4">
-      <div className="text-2xl font-semibold">
-        Iniciar sesión
-      </div>
-      <div>
-        <TextInput id="email1" type="email" placeholder="Correo electrónico" required />
-      </div>
-      <div>
-        <TextInput id="password1" placeholder="Contraseña" type="password" required />
-      </div>
-      <div className="flex items-center gap-2">
-        <Checkbox id="remember" />
-        <Label htmlFor="remember">Recuérdame</Label>
-      </div>
-      <Button color="purple" type="submit">Iniciar sesión</Button>
-    </form>
-    <div className="mt-1">
-      <Label className="text-sm font-extralight"> ¿No tienes una cuenta todavía? </Label>
-      <Button color="warning" href="/Auth/register">Registrarme</Button>
-    </div>
-  </Card>
-  }
+    return (
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <Card className="w-full max-w-sm p-4 shadow-lg rounded-lg bg-white">
+                <h2 className="text-3xl font-bold text-center mb-6">Iniciar sesión</h2>
+                <form className="flex flex-col gap-4">
+                    <div>
+                        <input id="email1" type="email" name="username" placeholder="Correo electrónico" className="block w-full rounded-lg border-gray-300 focus:border-[#7A4993] focus:ring-[#7A4993]" required />
+                    </div>
+                    <div>
+                        <input type="password" id="password1" name="password" placeholder="Contraseña" className="block w-full rounded-lg border-gray-300 focus:border-[#7A4993] focus:ring-[#7A4993]" required />
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <Checkbox id="remember" />
+                        <Label htmlFor="remember">Recuérdame</Label>
+                    </div>
+                    <button type="submit" className="relative flex items-stretch justify-center p-2 text-center font-medium transition-[color,background-color,border-color,text-decoration-color,fill,stroke,box-shadow] focus:z-10 focus:outline-none border border-transparent focus:ring-4 focus:ring-cyan-300 enabled:hover:bg-[#6b3e7b] rounded-lg bg-[#7A4993] hover:bg-[#6b3e7b] text-white">
+                        Iniciar Sesión
+                    </button>
+                </form>
+                <div className="mt-4 text-center">
+                    <Label className="text-sm font-light">
+                        ¿No tienes una cuenta todavía?{" "}
+                        <a href="/Auth/register" className="text-cyan-500 hover:underline">
+                            Registrarse
+                        </a>
+                    </Label>
+                </div>
+            </Card>
+        </div>
+    );
+}
