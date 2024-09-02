83df446ec8a66de3956cce982923bed8bca32ed1
andres-alfonso <falfonso@windowschannel.com>
2024-09-02 14:52:39 -0500

    Se cambia estructura de formularios

--- src/app/Components/AuthComponents/forgotPass.tsx
+++ src/app/Components/AuthComponents/forgotPass.tsx
@@ -1,99 +1,76 @@
 'use client';
 
 import React, { useState } from "react";
-import { Button, Card, Checkbox, Label, TextInput, Alert } from "flowbite-react"; // Añadido Alert
+import { Button, Card, Label, TextInput, Alert } from "flowbite-react";
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
+import { requestPasswordReset } from '@/api/auth';
 
-import { forgotPassword } from '../../../api/auth'; // Asegúrate de que la ruta sea correcta
-import { useAuthCheck } from "@/app/hooks/useAuthCheck";
-
-export default function ForgotPass() {
+export default function ForgotPassword() {
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
     const [error, setError] = useState('');
     const [isLoading, setIsLoading] = useState(false);
+    const router = useRouter();
 
-    const { isAuthenticated } = useAuthCheck(false);
-
-    if (isLoading) {
-        return <div>Cargando...</div>;
-    }
-
-    if (isAuthenticated) {
-        return null; // El hook se encargará de la redirección al dashboard
-    }
-
-    const handleSubmit = async (e) => {
+    const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         setIsLoading(true);
         setMessage('');
         setError('');
 
         try {
-            const result = await forgotPassword(email);
-            if (result.success) {
-                setMessage(result.message);
-                setEmail('');
-            } else {
-                setError(result.error);
-            }
+            await requestPasswordReset(email);
+            setMessage('Si existe una cuenta con este correo, recibirás un enlace para restablecer tu contraseña.');
+            setTimeout(() => router.push('/auth/access'), 5000);
         } catch (err) {
-            setError(err.message || 'Ocurrió un error. Por favor, intenta de nuevo.');
+            console.error('Error al solicitar restablecimiento de contraseña:', err);
+            setError('Ocurrió un error. Por favor, intenta de nuevo.');
         } finally {
             setIsLoading(false);
         }
     };
 
     return (
-        <>
-            <div className="flex justify-center items-center min-h-screen bg-gray-100">
-                <Card className="w-full max-w-sm p-4 shadow-lg rounded-lg bg-white">
-                    <h2 className="text-3xl font-bold text-center mb-2">¿Olvidaste tu contraseña?</h2>
-                    <h3 className="text-lg font-bold text-center text-slate-600 mb-3">Te enviaremos un link para el restablecimiento de tu contraseña</h3>
-                    {message && (
-                        <Alert color="success" className="mb-4">
-                            <span className="font-medium">{message}</span>
-                        </Alert>
-                    )}
-                    {error && (
-                        <Alert color="failure" className="mb-4">
-                            <span className="font-medium">{error}</span>
-                        </Alert>
-                    )}
-                    <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-                        <div className="mb-4">
-                            <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">Correo Electrónico</label>
-                            <input 
-                                id="email" 
-                                type="email" 
-                                name="email" 
-                                placeholder="tu@email.com"
-                                className="block w-full rounded-lg border-gray-300 focus:border-[#7A4993] focus:ring-[#7A4993]" 
-                                required 
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </div>
-                        <button type="submit" className="relative flex items-stretch justify-center p-2 text-center font-medium transition-[color,background-color,border-color,text-decoration-color,fill,stroke,box-shadow] focus:z-10 focus:outline-none border border-transparent focus:ring-4 focus:ring-cyan-300 enabled:hover:bg-[#6b3e7b] rounded-lg bg-[#7A4993] hover:bg-[#6b3e7b] text-white" disabled={isLoading}>
-                            {isLoading ? 'Enviando...' : 'Enviar Correo de Recuperación'}
-                        </button>
-                    </form>
-                    <span className="text-xs text-slate-600 mt-4">Regresar al{" "}
-                        <Link href="/auth/access" className="text-cyan-500 hover:underline">
-                            inicio de sesión
-                        </Link>
-                    </span>
-                    <div className="mt-4 text-center">
-                        <Label className="text-sm font-light">
-                            ¿No tienes una cuenta todavía?{" "}
-                            <Link href="/auth/access" className="text-cyan-500 hover:underline">
-                                Registrarse
-                            </Link>
-                        </Label>
-                    </div>
-                </Card>
+        <Card className="w-full max-w-sm p-4 shadow-lg rounded-lg bg-white">
+            <h2 className="text-3xl font-bold text-center mb-2">Olvidé mi Contraseña</h2>
+            <h3 className="text-lg font-bold text-center text-slate-600 mb-3">Ingresa tu correo electrónico para recibir un enlace de restablecimiento</h3>
+            {message && (
+                <Alert color="success" className="mb-4">
+                    <span className="font-medium">{message}</span>
+                </Alert>
+            )}
+            {error && (
+                <Alert color="failure" className="mb-4">
+                    <span className="font-medium">{error}</span>
+                </Alert>
+            )}
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+                <div className="mb-2">
+                    <Label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">
+                        Correo Electrónico
+                    </Label>
+                    <TextInput
+                        id="email"
+                        type="email"
+                        required
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="tucorreo@ejemplo.com"
+                    />
+                </div>
+                <Button type="submit" disabled={isLoading} className="enabled:hover:bg-[#6b3e7b] rounded-lg bg-[#7A4993] hover:bg-[#6b3e7b] text-white">
+                    {isLoading ? 'Enviando...' : 'Enviar Enlace de Restablecimiento'}
+                </Button>
+            </form>
+            <div className="mt-4 text-center">
+                <Label className="text-sm font-light">
+                    ¿Recordaste tu contraseña?{" "}
+                    <Link href="/auth/access" className="text-cyan-500 hover:underline">
+                        Iniciar sesión
+                    </Link>
+                </Label>
             </div>
-        </>
-    )
+        </Card>
+    );
 }
--- src/app/Components/AuthComponents/resertPass.tsx
+++ src/app/Components/AuthComponents/resertPass.tsx
@@ -7,30 +7,34 @@ import Link from 'next/link';
 import { resetPassword } from '@/api/auth';
 import { useAuthCheck } from "@/app/hooks/useAuthCheck";
 
-export default function ResetPassword() {
+interface ResetPasswordFormProps {
+  resetKey: string;
+}
+
+export default function ResetPasswordForm({ resetKey }: ResetPasswordFormProps) {
     const [password1, setPassword1] = useState('');
     const [password2, setPassword2] = useState('');
     const [message, setMessage] = useState('');
     const [error, setError] = useState('');
     const [isLoading, setIsLoading] = useState(false);
-    const [resetKey, setResetKey] = useState('');
+    const [uidb64, setUidb64] = useState('');
+    const [token, setToken] = useState('');
     const router = useRouter();
-    const { isAuthenticated} = useAuthCheck(false);
+    const { isAuthenticated } = useAuthCheck(false);
 
     useEffect(() => {
-        // Extraer la clave de restablecimiento de la URL
-        const pathParts = window.location.pathname.split('/');
-        const key = pathParts[pathParts.length - 1];
-        setResetKey(key);
-    }, []);
-
-
-    if (isLoading) {
-        return <div>Cargando...</div>;
-    }
+        const parts = resetKey.split('-');
+        if (parts.length >= 2) {
+            setUidb64(parts[0]);
+            setToken(parts.slice(1).join('-')); // Unir todas las partes restantes como token
+        } else {
+            setError('Link de restablecimiento inválido');
+        }
+    }, [resetKey]);
 
     if (isAuthenticated) {
-        return null; // El hook se encargará de la redirección al dashboard
+        router.push('/dashboard');
+        return null;
     }
 
     const handleSubmit = async (e: React.FormEvent) => {
@@ -45,81 +49,75 @@ export default function ResetPassword() {
             return;
         }
 
-        if (!resetKey) {
-            setError('Clave de restablecimiento no válida');
+        if (!uidb64 || !token) {
+            setError('Link de restablecimiento inválido');
             setIsLoading(false);
             return;
         }
 
         try {
-            const result = await resetPassword(resetKey, password1, password2);
-            if (result.success) {
-                setMessage('Tu contraseña ha sido restablecida exitosamente');
-                setTimeout(() => router.push('/auth/access'), 2500);
-            } else {
-                setError(result.error || 'Ocurrió un error al restablecer la contraseña');
-            }
-        } catch (err) {
-            console.error('Error durante el restablecimiento de contraseña:', err);
-            setError('Ocurrió un error. Por favor, intenta de nuevo.');
+            const response = await resetPassword(uidb64, token, password1);
+            setMessage('Tu contraseña ha sido restablecida exitosamente');
+            setTimeout(() => router.push('/auth/access'), 2500);
+        } catch (error: any) {
+            console.error('Error durante el restablecimiento de contraseña:', error);
+            setError(error.response?.data?.error || 'Ocurrió un error al restablecer la contraseña. Por favor, intenta de nuevo.');
         } finally {
             setIsLoading(false);
         }
     };
 
     return (
-        <div className="flex justify-center items-center min-h-screen bg-gray-100">
-            <Card className="w-full max-w-sm p-4 shadow-lg rounded-lg bg-white">
-                <h2 className="text-3xl font-bold text-center mb-2">Nueva Contraseña</h2>
-                <h3 className="text-lg font-bold text-center text-slate-600 mb-3">Ingresa una nueva contraseña que no hayas utilizado en otro sitio</h3>
-                {message && (
-                    <Alert color="success" className="mb-4">
-                        <span className="font-medium">{message}</span>
-                    </Alert>
-                )}
-                {error && (
-                    <Alert color="failure" className="mb-4">
-                        <span className="font-medium">{error}</span>
-                    </Alert>
-                )}
-                <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-                    <div className="mb-2">
-                        <Label htmlFor="password1" className="block text-sm font-medium text-gray-700 mb-1">
-                            Nueva Contraseña
-                        </Label>
-                        <TextInput
-                            id="password1"
-                            type="password"
-                            required
-                            value={password1}
-                            onChange={(e) => setPassword1(e.target.value)}
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <Label htmlFor="password2" className="block text-sm font-medium text-gray-700 mb-1">
-                            Confirmar Contraseña
-                        </Label>
-                        <TextInput
-                            id="password2"
-                            type="password"
-                            required
-                            value={password2}
-                            onChange={(e) => setPassword2(e.target.value)}
-                        />
-                    </div>
-                    <Button type="submit" disabled={isLoading || !resetKey}>
-                        {isLoading ? 'Procesando...' : 'Restablecer Contraseña'}
-                    </Button>
-                </form>
-                <div className="mt-4 text-center">
-                    <Label className="text-sm font-light">
-                        ¿Ya tienes una cuenta?{" "}
-                        <Link href="/auth/access" className="text-cyan-500 hover:underline">
-                            Iniciar sesión
-                        </Link>
+        <Card className="w-full max-w-sm p-4 shadow-lg rounded-lg bg-white">
+            <h2 className="text-3xl font-bold text-center mb-2">Nueva Contraseña</h2>
+            <h3 className="text-lg font-bold text-center text-slate-600 mb-3">Ingresa una nueva contraseña que no hayas utilizado en otro sitio</h3>
+            {message && (
+                <Alert color="success" className="mb-4">
+                    <span className="font-medium">{message}</span>
+                </Alert>
+            )}
+            {error && (
+                <Alert color="failure" className="mb-4">
+                    <span className="font-medium">{error}</span>
+                </Alert>
+            )}
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+                <div className="mb-2">
+                    <Label htmlFor="password1" className="block text-sm font-medium text-gray-700 mb-1">
+                        Nueva Contraseña
                     </Label>
+                    <TextInput
+                        id="password1"
+                        type="password"
+                        required
+                        value={password1}
+                        onChange={(e) => setPassword1(e.target.value)}
+                    />
                 </div>
-            </Card>
-        </div>
+                <div className="mb-4">
+                    <Label htmlFor="password2" className="block text-sm font-medium text-gray-700 mb-1">
+                        Confirmar Contraseña
+                    </Label>
+                    <TextInput
+                        id="password2"
+                        type="password"
+                        required
+                        value={password2}
+                        onChange={(e) => setPassword2(e.target.value)}
+                    />
+                </div>
+                <Button type="submit" disabled={isLoading} className="enabled:hover:bg-[#6b3e7b] rounded-lg bg-[#7A4993] hover:bg-[#6b3e7b] text-white">
+                    {isLoading ? 'Procesando...' : 'Restablecer Contraseña'}
+                </Button>
+            </form>
+            <div className="mt-4 text-center">
+                <Label className="text-sm font-light">
+                    ¿Ya tienes una cuenta?{" "}
+                    <Link href="/auth/access" className="text-cyan-500 hover:underline">
+                        Iniciar sesión
+                    </Link>
+                </Label>
+            </div>
+        </Card>
     );
 }
