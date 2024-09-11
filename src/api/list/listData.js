f846aa01043a5cac7ceaeccc2dc28fd0992053d3
santiago castro <scastro@windowschannel.com>
2024-09-11 16:29:33 -0500

    se cambian las urls

--- src/api/list/listData.js
+++ src/api/list/listData.js
@@ -55,7 +55,7 @@ export const fetchSummaryCardsData = async (institutionId, date) => {
 
 export const fetchCategories = async () => {
   try {
-    const response = await axios.get(`${API_URL}/api/social-metrics/institutions/salud/categories`);
+    const response = await axios.get(`${API_URL}/api/social-metrics/institutions/categories/salud`);
     return response.data;
   } catch (error) {
     console.error('Error fetching categories:', error);
