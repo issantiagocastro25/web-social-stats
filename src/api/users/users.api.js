bac9e63cbece5ab927071fd89c0709631313c358
andres-alfonso <falfonso@windowschannel.com>
2024-09-19 15:43:09 -0500

    Se habilita chequeo de sesion

--- src/api/users/users.api.js
+++ src/api/users/users.api.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 
 
 export const getAllUsers = async () => {
-    const response = await api.get('/api/app/users');
+    const response = await api.get('/api/user/get-users/');
     return response.data;
   };
