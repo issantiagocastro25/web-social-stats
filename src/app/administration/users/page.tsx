d6942139c8c43f4d9f65ae4b16323158274c2cc4
andres-alfonso <falfonso@windowschannel.com>
2024-10-02 15:13:21 -0500

    Se limita la muestra del email

--- src/app/administration/users/page.tsx
+++ src/app/administration/users/page.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 export default function page() {
   return (
     <>
+      <title>Users</title>
         <div className="bg-[#f6f3fa] h-screen">
             <div className="container mx-auto p-4 flex items-center justify-center">
                 <div className="max-w-6xl w-full pt-10">
