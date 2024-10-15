357aa31d64dd436f67c89e42830b24a197c15a56
andres-alfonso <falfonso@windowschannel.com>
2024-10-15 07:35:11 -0500

    Se ajusta api

--- src/api/index.js
+++ src/api/index.js
@@ -2,21 +2,19 @@ import axios from 'axios';
 
 function getCookie(name) {
   let cookieValue = null;
-  if (typeof document !== 'undefined') {
-    if (document.cookie && document.cookie !== '') {
-      const cookies = document.cookie.split(';');
-      for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i].trim();
-        if (cookie.substring(0, name.length + 1) === (name + '=')) {
-          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-          break;
-        }
+  if (document.cookie && document.cookie !== '') {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
       }
     }
   }
   return cookieValue;
 }
-1
+
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
@@ -28,5 +26,8 @@ api.interceptors.request.use((config) => {
   config.headers['X-CSRFToken'] = getCookie('csrftoken');
   return config;
 });
+
+// api.defaults.xsrfCookieName = 'csrftoken';
+// api.defaults.xsrfHeaderName = 'X-CSRFToken';
 
 export default api;
