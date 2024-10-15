7b9948a4ec6e5b1a8c0cc26a1f9060ab959780b1
andres-alfonso <falfonso@windowschannel.com>
2024-10-15 11:18:26 -0500

    Se ajusta api

--- src/api/index.js
+++ src/api/index.js
@@ -1,39 +1,22 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-function getCookie(name) {
-  const csrfToken = Cookies.get('csrftoken');
-
-  console.log(csrfToken);
-  let cookieValue = null;
-  if (typeof document !== 'undefined') {
-    if (document.cookie && document.cookie !== '') {
-      const cookies = document.cookie.split(';');
-      for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i].trim();
-        if (cookie.substring(0, name.length + 1) === (name + '=')) {
-          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-          break;
-        }
-      }
-    }
-  }
-  return cookieValue;
-}
-
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  withCredentials: true,
+  withCredentials: true, // Asegúrate de que se envíen las credenciales
 });
 
 // Interceptor para agregar el token CSRF
 api.interceptors.request.use((config) => {
-  const csrfToken = getCookie('csrftoken');
-  if (csrfToken) {
-    config.headers['X-CSRFToken'] = csrfToken;
-  }
+  const csrfToken = Cookies.get('csrftoken'); // Obtener el token CSRF usando js-cookie
   console.log(csrfToken);
+  if (csrfToken) {
+    config.headers['X-CSRFToken'] = csrfToken; // Establecer el encabezado CSRF
+  }
   return config;
+}, (error) => {
+  // Manejo de errores en la solicitud
+  return Promise.reject(error);
 });
 
 export default api;
