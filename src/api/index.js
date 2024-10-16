49f15ef5f0287a1813d63664be89c589a9a1f06d
andres-alfonso <falfonso@windowschannel.com>
2024-10-16 08:33:12 -0500

    Se vueve a cambiar index de api

--- src/api/index.js
+++ src/api/index.js
@@ -22,43 +22,80 @@
 // export default api;
 
 
+// import axios from 'axios';
+// import Cookies from 'js-cookie';
+
+// const api = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_API_URL,
+//   withCredentials: true,
+// });
+
+// // Function to get CSRF token
+// const getCSRFToken = async () => {
+//   try {
+//     const response = await axios.get('https://api-stats.colombiaredessociales.com/api/getCsrf-token/');
+//     return response.data.csrfToken;
+//   } catch (error) {
+//     console.error('Error fetching CSRF token:', error);
+//     return null;
+//   }
+// };
+
+// // Interceptor to add CSRF token to requests
+// api.interceptors.request.use(async (config) => {
+//   let csrfToken = Cookies.get('csrftoken');
+  
+//   if (!csrfToken) {
+//     csrfToken = await getCSRFToken();
+//     if (csrfToken) {
+//       Cookies.set('csrftoken', csrfToken);
+//     }
+//   }
+
+//   if (csrfToken) {
+//     config.headers['X-CSRFToken'] = csrfToken;
+//   }
+  
+//   return config;
+// }, (error) => {
+//   return Promise.reject(error);
+// });
+
+// export default api;
+
+
+
 import axios from 'axios';
-import Cookies from 'js-cookie';
+
+function getCookie(name) {
+  let cookieValue = null;
+  if (typeof document !== 'undefined') {
+    if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.substring(0, name.length + 1) === (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+  }
+  return cookieValue;
+}
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
+  xsrfHeaderName: 'X-CSRFToken',
+  xsrfCookieName: 'csrftoken',
 });
 
-// Function to get CSRF token
-const getCSRFToken = async () => {
-  try {
-    const response = await axios.get('https://api-stats.colombiaredessociales.com/api/getCsrf-token/');
-    return response.data.csrfToken;
-  } catch (error) {
-    console.error('Error fetching CSRF token:', error);
-    return null;
-  }
-};
-
-// Interceptor to add CSRF token to requests
-api.interceptors.request.use(async (config) => {
-  let csrfToken = Cookies.get('csrftoken');
-  
-  if (!csrfToken) {
-    csrfToken = await getCSRFToken();
-    if (csrfToken) {
-      Cookies.set('csrftoken', csrfToken);
-    }
-  }
-
-  if (csrfToken) {
-    config.headers['X-CSRFToken'] = csrfToken;
-  }
-  
+api.interceptors.request.use((config) => {
+  config.headers['X-CSRFToken'] = getCookie('csrftoken');
   return config;
-}, (error) => {
-  return Promise.reject(error);
 });
 
+api.defaults.xsrfCookieName = 'csrftoken';
+api.defaults.xsrfHeaderName = 'X-CSRFToken';
 export default api;
